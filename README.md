2021ecc30bc49ce574a4d8550b3bfe466ce1f389
mayankshukla27 <97962797+mayankshukla27@users.noreply.github.com>
2022-02-20 16:30:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# basicbankingsystem.github.io
+# BasicBankingSystem
+***I have created the Basic Banking System as an intern for the Sparks Foundation.***
+#GRIPFEB22 
+
+Intern @ Spark Foundation
+
+Task 1: Basic Banking System
+
+The Basic Banking System website function is to transfer money from one account to another account.
+
+**FrontEnd: HTML, CSS, JS ***
+***Backend: PHP***
+***Database: MySQL***
+      
---
+++ navbar.php
@@ -0,0 +1,19 @@
+
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color :#2c146b;"> <img src="img/img2.png" alt="logo" height=50px width=50px><b>BANK OF SPARKS</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color :   #371790;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :  #371790;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color :  #371790;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
